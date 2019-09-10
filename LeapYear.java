eb6a6179c6a2ba25c99a1f1e3f31eb99ca4c2eb9
Juan Carlos <jc.espinosa.ceniceros@gmail.com>
2019-09-09 20:24:49 -0500

    Codigo inicial

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+class LeapYear {
+
+    private static boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
+        // TODO
+        return isLeapYear;
+    }
+
+    public static void main(String[] args) {
+
+        int year = 0;
+        
+        // TODO : Get args year
+        // year = 
+
+        System.out.println(isLeapYear(year));
+
+    }
+
+}
